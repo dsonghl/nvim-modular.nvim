05b53a14d0494e0cd89312b5f2911962ee24a5e5
dean.song <dean.song@epicgames.com>
2024-09-05 10:14:15 +0800

    feat: Neovim 0.10 updates (nvim-lua#936)

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -19,11 +19,9 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to force a plugin to be loaded.
   --
-  --  This is equivalent to:
-  --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  -- { 'numToStr/Comment.nvim', opts = {} },
 
   require 'plugins.gitsigns',
 
