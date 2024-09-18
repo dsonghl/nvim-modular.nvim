efb1e879dfb8abfa230174190dd3814c8bbfb1d7
dean.song <dean.song@epicgames.com>
2024-09-18 10:56:21 +0800

    fix: comment out the ess/jk for terminal keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -67,14 +67,14 @@ vim.keymap.set('n', '<leader>k', '<CMD>bdelete<CR>', { desc = 'Kill Buffer' })
 
 -- [[ Terminal mappings ]]
 -- https://medium.com/@shaikzahid0713/terminal-support-in-neovim-c616923e0431
--- function _G.set_terminal_keymaps()
---   local opts = { noremap = true }
---   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
---   -- vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
---   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
---   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
---   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
---   vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
--- end
---
--- vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
+function _G.set_terminal_keymaps()
+  local opts = { noremap = true }
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+end
+
+vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
